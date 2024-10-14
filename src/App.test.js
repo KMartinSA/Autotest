0a6ad81d22843b8a6ab54eae226ad7d767dc78a5
Kyle <kylethedon2786@gmail.com>
2024-10-14 15:09:55 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const txtElement = screen.getByText(/hello/i);
+  expect(txtElement).toBeInTheDocument();
 });
